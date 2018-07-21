898aacd7045677a7277476e0b0f24e2c50c92462
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-21 10:53:42 +0900

    growing

--- src/ddff-windbarb.rb
+++ src/ddff-windbarb.rb
@@ -9,11 +9,23 @@ class WindBarb
     @img = Draw.new(64)
   end
 
+  
+
   def run
     @img.center
     upwind = @d * 10 - 90
     upwind += 360 if upwind < 0
     @img.line upwind, 25
+    case @f
+    when 1...8
+      @img.center
+      @img.move upwind, 25
+      @img.line upwind + 120, 3
+    when 8...48
+    when 48...98
+    when 98...148
+    else raise 'i dunno'
+    end
     @img.savepng @fn
     $stderr.puts @fn if $stderr.tty?
   end
