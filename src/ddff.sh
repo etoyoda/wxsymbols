337c9b97ecdbf6db34a28f706438d673756ace16
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-21 01:25:56 +0000

    testing settigs

--- src/ddff.sh
+++ src/ddff.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 dd=00
-while (( ++dd <= 36 ))
+while (( ++dd <= 1 ))
 do
   ff=00
   while (( ( ff += 5 ) <= 95 ))
   do
-    ruby -I. ddff-windbarb.rb $dd $ff d${dd}f${ff}.png
+    ruby -I. ddff-windbarb.rb $dd $ff ../img/d${dd}f${ff}.png
   done
 done
